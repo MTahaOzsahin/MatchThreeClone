8904e40de1913f23bdaa618b3c55be06ba3fcd57
MTahaOzsahin <87945619+MTahaOzsahin@users.noreply.github.com>
2023-05-31 21:08:41 +0300

    Added Move Function.

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,55 @@ public class GamePiece : MonoBehaviour
     public int xIndex;
     public int yIndex;
 
+    private bool _isMoving;
+
     public void SetCoordinates(int x,int y)
     {
         xIndex = x;
         yIndex = y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Mathf.RoundToInt(transform.position.x + 1), Mathf.RoundToInt(transform.position.y), 0.3f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Mathf.RoundToInt(transform.position.x - 1), Mathf.RoundToInt(transform.position.y), 0.3f);
+        }
+    }
+
+    public void Move(int destX, int destY,float timeToMove)
+    {
+        if (!_isMoving) 
+            StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
+    }
+
+    private IEnumerator MoveRoutine(Vector3 destination, float timeToMove)
+    {
+        var startPosition = transform.position;
+        var reachedDestination = false;
+        var elapsedTime = 0f;
+        _isMoving = true;
+        while (!reachedDestination)
+        {
+            if (Vector3.Distance(transform.position,destination)< 0.01f)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoordinates((int)destination.x,(int)destination.y);
+            }
+
+            elapsedTime += Time.deltaTime;
+            var t = Mathf.Clamp(elapsedTime / timeToMove,0,1);
+            t = Mathf.Sin(t * Mathf.PI * 0.5f);
+             transform.position = Vector3.Lerp(startPosition, destination, t);
+            yield return new WaitForEndOfFrame();
+        }
+
+        _isMoving = false;
     }
 }
